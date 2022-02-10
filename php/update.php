c67fa84160417b544f1344001106d0889983f719
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-02-10 12:29:50 +0600

    Insert github

---
+++ php/update.php
@@ -0,0 +1,23 @@
+<?php
+include "config.php";
+
+if(isset($_GET['id'])){
+    $update_id = mysqli_real_escape_string($conn, $_GET['id']);
+    $sql = mysqli_query($conn, "UPDATE FROM url WHERE shorten_url = '{$update_id}'");
+    if($sql){
+        header("Location: ../");
+    }else{
+        header("Location: ../");
+    }
+}elseif(isset($_GET['delete'])){
+    $sql3 = mysqli_query($conn, "DELETE FROM url");
+    if($sql3){
+        header("Location: ../");
+    }else{
+        header("Location: ../");
+    }
+}else{
+    header("Location: ../");
+}
+
+?>
